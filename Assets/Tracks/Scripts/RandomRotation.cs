99fb2ac6e8a530b83711a364f6ec61824eaaa6ae
Johnny Howe <jonathon.o.howe@gmail.com>
2021-08-10 21:18:04 +1200

    Racetrack surrounds 1. REQURES EXTERNAL PACKAGE RIGHT NOW
    Few trees, seats, tyres

---
+++ Assets/Tracks/Scripts/RandomRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    public bool lockX = false;
+    public bool lockY = false;
+    public bool lockZ = false;
+    public int min = 0;
+    public int max = 360;
+
+    void Awake()
+    {
+        transform.eulerAngles = new Vector3(
+            lockX ? transform.eulerAngles.x : GetRandomRotation(),
+            lockY ? transform.eulerAngles.y : GetRandomRotation(),
+            lockZ ? transform.eulerAngles.z : GetRandomRotation()
+        );
+    }
+
+    int GetRandomRotation() {
+        return Random.Range(min, max);
+    }
+}
