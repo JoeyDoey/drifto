a5ffe4e9730c9e21c090ae7ea1add17cf20dbe23
Johnny Howe <jonathon.o.howe@gmail.com>
2021-08-25 18:29:44 +1200

    When off track for too long, game restarts

--- Assets/GameController/GameController.cs
+++ Assets/GameController/GameController.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public RoadCheck carCheck;
-    public UnityEvent offTrack;
-    public UnityEvent onTrack;
+    public Text timeLeftText;
+    public Text getBackOnRoadText;
+    public float maxOffTrackTime = 2;
+    public float currentOffTrackTime;
 
     bool last = false;
     void Update()
     {
         bool next = carCheck.IsOnRoad();
-        if (next && !last) onTrack.Invoke(); 
-        if (!next && last) offTrack.Invoke(); 
+        if (next && !last) OnTrack(); 
+        if (!next && last) OffTrack();
         last = next;
+        if (!next) currentOffTrackTime = Mathf.Max(currentOffTrackTime - Time.deltaTime, 0);
+
+        if (currentOffTrackTime <= 0) OnTimeout();
+        timeLeftText.text = currentOffTrackTime.ToString();
+    }
+
+    void OffTrack() {
+        currentOffTrackTime = maxOffTrackTime;
+        timeLeftText.gameObject.SetActive(true);
+        getBackOnRoadText.gameObject.SetActive(true);
+    }
+
+    void OnTrack() {
+        timeLeftText.gameObject.SetActive(false);
+        getBackOnRoadText.gameObject.SetActive(false);
+        currentOffTrackTime = maxOffTrackTime;
+    }
+
+    void OnTimeout() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
