cf223bc690bcac66690f6ae5a5ad8c6d948d94fe
Johnny Howe <jonathon.o.howe@gmail.com>
2021-08-25 21:46:38 +1200

    Restart game on collision with barriers

---
+++ Assets/Scripts/CarCollisionCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarCollisionCheck : MonoBehaviour
+{
+    public LayerMask collidableLayer;
+    public float maxImpulse = 1;
+
+    Rigidbody rb;
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if ((collidableLayer.value & 1 << collisionInfo.gameObject.layer) == 1 << collisionInfo.gameObject.layer)
+        {
+            if (collisionInfo.impulse.magnitude / rb.mass >= maxImpulse)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
