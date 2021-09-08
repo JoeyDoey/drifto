e4998ade7ddb2c78f4ca71ab10efb69f5575f859
Johnny Howe <jonathon.o.howe@gmail.com>
2021-09-09 07:18:44 +1200

    Clipping point text fades out

--- Assets/ClippingPoint/ClippingPointUI.cs
+++ Assets/ClippingPoint/ClippingPointUI.cs
@@ -6,23 +6,37 @@ using UnityEngine.UI;
 public class ClippingPointUI : MonoBehaviour
 {
     public Text[] texts;
+    float[] timers;
+    public float displayTime = 1;
     public int pointsPerBurst = 3;
+    public Color baseColor = Color.white;
 
     void Start()
     {
-        foreach (Text text in texts)
+        timers = new float[pointsPerBurst];
+        for (int i = 0; i < pointsPerBurst; i++)
         {
-            text.gameObject.SetActive(false);
+            timers[i] = 0;
         }
     }
 
     void Update()
     {
+        for (int i = 0; i < texts.Length; i++)
+        {
+            timers[i] = Mathf.Max(0, timers[i] - Time.deltaTime);
+            texts[i].color = new Color(baseColor.r, baseColor.g, baseColor.b, baseColor.a * FadeFunction(timers[i] / displayTime));
+        }
     }
 
     public void Set(int newScore, int index)
     {
-        texts[index % pointsPerBurst].gameObject.SetActive(true);
         texts[index % pointsPerBurst].text = newScore.ToString();
+        timers[index % pointsPerBurst] = displayTime;
+    }
+
+    float FadeFunction(float t)
+    {
+        return 1 - Mathf.Pow(1 - t, 10);
     }
 }
