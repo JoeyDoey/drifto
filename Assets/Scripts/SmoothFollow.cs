d532a6133980696c4bac1b912769e2f90ccfae4f
Johnny Howe <jonathon.o.howe@gmail.com>
2021-09-10 15:39:44 +1200

    Nicer camera movement

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,33 @@
+﻿// Smooth Follow from Standard Assets
+// Converted to C# because I fucking hate UnityScript and it's inexistant C# interoperability
+// If you have C# code and you want to edit SmoothFollow's vars ingame, use this instead.
+using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour {
+    
+	// The target we are following
+	public Transform target;
+	// The distance in the x-z plane to the target
+	public float distance = 10.0f;
+	// the height we want the camera to be above the target
+	public float height = 5.0f;
+	// How much we 
+    public float smoothTime = 0.3f;
+    Vector3 velocity;
+
+	// Place the script in the Camera-Control group in the component menu
+	[AddComponentMenu("Camera-Control/Smooth Follow")]
+
+	void LateUpdate () {
+		// Early out if we don't have a target
+		if (!target) return;
+
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, height, -distance));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+	
+		// Always look at the target
+		transform.LookAt(target);
+	}
+}
