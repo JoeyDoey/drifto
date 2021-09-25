418b5c1c1c696dc8d798102b80d876198c8a5c14
Johnny Howe <jonathon.o.howe@gmail.com>
2021-09-26 10:23:39 +1300

    Clipping points fall when hit

---
+++ Assets/ClippingPoint/ClippingPointFall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClippingPointFall : MonoBehaviour
+{
+    public Animator animator;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            animator.SetTrigger("fall");
+        }
+    }
+}
