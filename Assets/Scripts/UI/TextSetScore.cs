ee9504f532a4bf226cb03184ffbb27a617ac6784
Johnny Howe <jonathon.o.howe@gmail.com>
2021-11-06 09:04:10 +1300

    Refactor file structure

---
+++ Assets/Scripts/UI/TextSetScore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextSetScore : MonoBehaviour
+{
+    [System.Serializable]
+    public enum ScoreType
+    {
+        score,
+        multiplier,
+    }
+
+    public string prefix = "";
+    public ScoreType scoreType;
+    TextMeshProUGUI textPro;
+    public GameController.AScoreController scoreHandler;
+
+    private void Awake() {
+        textPro = GetComponent<TextMeshProUGUI>();     
+    }
+
+    void Update()
+    {
+        if (textPro) textPro.text = prefix + Mathf.Floor(GetScore()).ToString();
+    }
+
+    float GetScore()
+    {
+        switch (scoreType)
+        {
+            case ScoreType.score:
+                return scoreHandler.GetScore();
+            case ScoreType.multiplier:
+                return scoreHandler.GetMultiplier();
+        }
+        return scoreHandler.GetScore();
+    }
+}
